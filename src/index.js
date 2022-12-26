82e92f02f3ba021bafc6faf984df0b03a6cd544b
rivuff <rivunaskar0@gmail.com>
2022-12-26 13:46:36 +0530

    added roles and user_roles throgh table

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 // const userRepository = require('./repository/user-repository')
-
+const db = require('./models/index')
 const app =express();
 const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index')
 const userService = require('./services/user-service');
+const sequelize = require('sequelize');
+const UserRepository = require('./repository/user-repository');
+const {User,Role} = require('./models/index');
 
 const prepareAndStartServer = ()=>{
     app.use(bodyParser.json());
@@ -16,7 +19,15 @@ const prepareAndStartServer = ()=>{
     app.listen(PORT, async ()=>{
         console.log(`Server started at ${PORT}`);
         
-        const service = new userService();
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({ alter: true })
+        }
+
+        // const u1 = await User.findByPk(4);
+        // const r1 = await Role.findByPk(3);
+        //u1.addRole(r1)
+
+        //const service = new userService();
         // const newToken = service.createToken({email:'rivunaskar0@gmail.com', id:'1'});
         //console.log("new token is",newToken);
 
